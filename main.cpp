159f1a0e1a52beae0de865d99d795885ed25df40
Serdar Gülmez <serdar_gulmez@outlook.com>
2025-03-22 18:47:05 +0300

    tra_03

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 void printMenu()
@@ -93,15 +95,42 @@ void processUserOption(int userOption)
 int main()
 {
     cout << "--- Program is started!!! ---" << endl;
-    
-    while(true)
-    {
-        
-        printMenu();
-        int userOption = getUSerOption();
-        processUserOption(userOption);
 
-    }
+    enum class OrderBookType{bid, ask};
+
+    // double price = 5319.450228;
+    // double amount = 0.00020075;        
+    // string timestamp{"2020/03/17 17:01:24.884492"};
+    // string product{"BTC/USDT"};
+    // string orderType{"bid"};
+
+    vector<double> prices;
+    vector<double> amounts;
+    vector<string> timestamps;
+    vector<string> products;
+    vector<OrderBookType> orderTypes;
+
+    prices.push_back(5000.01);
+    amounts.push_back(0.001);
+    timestamps.push_back("2020/03/17 17:01:24.884492");
+    products.push_back("BTC/USDT");
+    orderTypes.push_back(OrderBookType::bid);
+
+    prices.push_back(10.01);
+    amounts.push_back(0.001);
+    timestamps.push_back("2020/03/17 17:01:24.884492");
+    products.push_back("BTC/USDT");
+    orderTypes.push_back(OrderBookType::bid);
+
+    cout << "prices: " << prices[0] << endl;
+    cout << "prices: " << prices[1] << endl;
+
+    // while(true)
+    // {
+    //     printMenu();
+    //     int userOption = getUSerOption();
+    //     processUserOption(userOption);
+    // }
 
     return 0;
 }
